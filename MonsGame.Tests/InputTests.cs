251067894f1bc0305887dbd8e2e58fc638fb8523
ivan grachev <ivan@ivan.lol>
2024-01-29 19:13:54 +0300

    add input model

--- MonsGame.Tests/InputTests.cs
+++ MonsGame.Tests/InputTests.cs
@@ -2,3 +2,42 @@
 
 using System.Text.Json;
 using MonsGame;
+
+public class InputTests
+{
+    [Fact]
+    public void LocationInput_Constructor_AssignsLocation()
+    {
+        var location = new Location(0, 0);
+        var locationInput = new Input.LocationInput(location);
+
+        Assert.Equal(location, locationInput.Location);
+    }
+
+    [Fact]
+    public void LocationInput_Equals_WithNull_ReturnsFalse()
+    {
+        var locationInput = new Input.LocationInput(new Location(0, 0));
+
+        Assert.False(locationInput.Equals(null));
+    }
+
+    [Fact]
+    public void LocationInput_GetHashCode_ConsistentOutput()
+    {
+        var location = new Location(0, 0);
+        var locationInput = new Input.LocationInput(location);
+        var expectedHashCode = HashCode.Combine(location);
+
+        Assert.Equal(expectedHashCode, locationInput.GetHashCode());
+    }
+
+    [Fact]
+    public void ModifierInput_Constructor_AssignsModifier()
+    {
+        var modifierInput = new Input.ModifierInput(Modifier.SelectPotion);
+
+        Assert.Equal(Modifier.SelectPotion, modifierInput.Modifier);
+    }
+
+}
--- MonsGame/Input.cs
+++ MonsGame/Input.cs
@@ -1,3 +1,59 @@
 // ∅ 2024 super-metal-mons
 
 namespace MonsGame;
+
+public enum Modifier
+{
+    SelectPotion,
+    SelectBomb,
+    Cancel
+}
+
+public abstract class Input
+{
+    public class LocationInput : Input, IEquatable<LocationInput>
+    {
+        public Location Location { get; }
+
+        public LocationInput(Location location)
+        {
+            Location = location;
+        }
+
+        public bool Equals(LocationInput other)
+        {
+            if (other is null) return false;
+            return Location.Equals(other.Location);
+        }
+
+        public override bool Equals(object obj) => Equals(obj as LocationInput);
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Location);
+        }
+    }
+
+    public class ModifierInput : Input, IEquatable<ModifierInput>
+    {
+        public Modifier Modifier { get; }
+
+        public ModifierInput(Modifier modifier)
+        {
+            Modifier = modifier;
+        }
+
+        public bool Equals(ModifierInput other)
+        {
+            if (other is null) return false;
+            return Modifier == other.Modifier;
+        }
+
+        public override bool Equals(object obj) => Equals(obj as ModifierInput);
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Modifier);
+        }
+    }
+}
