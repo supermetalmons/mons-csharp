3cfd42177eec4baa20b8d50172934f0c7d39da53
ivan grachev <ivan@ivan.lol>
2024-01-29 17:27:37 +0300

    add models and tests

--- MonsGame.Tests/BoardTests.cs
+++ MonsGame.Tests/BoardTests.cs
@@ -2,3 +2,31 @@
 
 using System.Text.Json;
 using MonsGame;
+
+public class BoardTests
+{
+    [Fact]
+    public void Constructor_WithNullItems_SetsInitialItems()
+    {
+        var board = new Board(null);
+        Assert.Equal(Config.InitialItems, board.Items);
+    }
+
+    [Fact]
+    public void SquareAt_ReturnsCorrectSquare()
+    {
+        var board = new Board();
+        var location = new Location(4, 4);
+        var expectedSquare = Square.Regular; // Or other Square type based on your test setup
+        Assert.Equal(expectedSquare, board.SquareAt(location));
+    }
+
+    [Fact]
+    public void AllMonsBases_ReturnsAllMonBaseLocations()
+    {
+        var board = new Board();
+        var monBaseLocations = board.AllMonsBases;
+        Assert.Contains(new Location(0, 5), monBaseLocations);
+    }
+
+}
--- MonsGame/Board.cs
+++ MonsGame/Board.cs
@@ -1,3 +1,65 @@
 // ∅ 2024 super-metal-mons
 
 namespace MonsGame;
+
+using System.Collections.Generic;
+
+public class Board
+{
+    private Dictionary<Location, Item> _items;
+
+    public IReadOnlyDictionary<Location, Item> Items { get { return _items; } }
+
+    public Board(Dictionary<Location, Item> items = null)
+    {
+        _items = items ?? Config.InitialItems;
+    }
+
+    public void RemoveItem(Location location)
+    {
+        _items.Remove(location);
+    }
+
+    public void Put(Item item, Location location)
+    {
+        _items[location] = item;
+    }
+
+    public Item GetItem(Location location)
+    {
+        _items.TryGetValue(location, out Item item);
+        return item;
+    }
+
+    public Square SquareAt(Location location)
+    {
+        return Config.Squares.TryGetValue(location, out Square square) ? square : Square.Regular;
+    }
+
+    public IEnumerable<Location> AllMonsBases
+    {
+        get
+        {
+            return Config.Squares
+                .Where(pair => pair.Value.Type == SquareType.MonBase)
+                .Select(pair => pair.Key);
+        }
+    }
+
+    public Location SupermanaBase
+    {
+        get
+        {
+            var baseLocation = Config.Squares
+                .FirstOrDefault(pair => pair.Value.Type == SquareType.SupermanaBase);
+
+            if (baseLocation.Equals(default(KeyValuePair<Location, Square>)))
+            {
+                throw new InvalidOperationException("Supermana base not found.");
+            }
+
+            return baseLocation.Key;
+        }
+    }
+
+}
