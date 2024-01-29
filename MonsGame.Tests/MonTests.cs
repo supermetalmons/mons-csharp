09bec12912dfc8202a4d935f88ad2e21b02a18ed
ivan grachev <ivan@ivan.lol>
2024-01-29 15:07:50 +0300

    add mon model and more tests

---
+++ MonsGame.Tests/MonTests.cs
@@ -0,0 +1,106 @@
+// ∅ 2024 super-metal-mons
+
+using System.Text.Json;
+using MonsGame;
+
+public class MonTests
+{
+    [Theory]
+    [InlineData(Mon.Kind.Demon, Color.White)]
+    [InlineData(Mon.Kind.Drainer, Color.Black)]
+    public void Constructor_SetsPropertiesCorrectly(Mon.Kind kind, Color color)
+    {
+        var mon = new Mon(kind, color, 1);
+
+        Assert.Equal(kind, mon.kind);
+        Assert.Equal(color, mon.color);
+        Assert.Equal(1, mon.cooldown);
+    }
+
+    [Fact]
+    public void Faint_SetsCooldownToTwo()
+    {
+        var mon = new Mon(Mon.Kind.Drainer, Color.Black);
+        mon.Faint();
+
+        Assert.Equal(2, mon.cooldown);
+    }
+
+    [Fact]
+    public void DecreaseCooldown_DecreasesCooldown()
+    {
+        var mon = new Mon(Mon.Kind.Angel, Color.White, 2);
+        mon.DecreaseCooldown();
+
+        Assert.Equal(1, mon.cooldown);
+    }
+
+    [Fact]
+    public void DecreaseCooldown_DoesNotDecreaseBelowZero()
+    {
+        var mon = new Mon(Mon.Kind.Spirit, Color.Black);
+        mon.DecreaseCooldown();
+
+        Assert.Equal(0, mon.cooldown);
+    }
+
+    [Fact]
+    public void IsFainted_ReturnsTrueWhenCooldownGreaterThanZero()
+    {
+        var mon = new Mon(Mon.Kind.Mystic, Color.White, 1);
+
+        Assert.True(mon.isFainted);
+    }
+
+    [Fact]
+    public void IsFainted_ReturnsFalseWhenCooldownIsZero()
+    {
+        var mon = new Mon(Mon.Kind.Mystic, Color.Black);
+
+        Assert.False(mon.isFainted);
+    }
+
+    [Theory]
+    [InlineData(Mon.Kind.Demon, Color.White)]
+    [InlineData(Mon.Kind.Angel, Color.Black)]
+    public void Equals_ReturnsTrueForEqualMons(Mon.Kind kind, Color color)
+    {
+        var mon1 = new Mon(kind, color);
+        var mon2 = new Mon(kind, color);
+
+        Assert.True(mon1.Equals(mon2));
+    }
+
+    [Fact]
+    public void Equals_ReturnsFalseForDifferentMons()
+    {
+        var mon1 = new Mon(Mon.Kind.Demon, Color.White);
+        var mon2 = new Mon(Mon.Kind.Angel, Color.Black);
+
+        Assert.False(mon1.Equals(mon2));
+    }
+
+    [Fact]
+    public void EqualityOperators_WorkCorrectly()
+    {
+        var mon1 = new Mon(Mon.Kind.Spirit, Color.White);
+        var mon2 = new Mon(Mon.Kind.Spirit, Color.White);
+        var mon3 = new Mon(Mon.Kind.Mystic, Color.Black);
+
+        Assert.True(mon1 == mon2);
+        Assert.False(mon1 == mon3);
+        Assert.False(mon1 != mon2);
+        Assert.True(mon1 != mon3);
+    }
+
+    [Theory]
+    [InlineData(Mon.Kind.Demon, Color.White)]
+    [InlineData(Mon.Kind.Angel, Color.Black)]
+    public void GetHashCode_ReturnsSameValueForEqualObjects(Mon.Kind kind, Color color)
+    {
+        var mon1 = new Mon(kind, color);
+        var mon2 = new Mon(kind, color);
+
+        Assert.Equal(mon1.GetHashCode(), mon2.GetHashCode());
+    }
+}
--- MonsGame/Mon.cs
+++ MonsGame/Mon.cs
@@ -1,3 +1,72 @@
 // ∅ 2024 super-metal-mons
 
 namespace MonsGame;
+
+public struct Mon : IEquatable<Mon>
+{
+    public enum Kind
+    {
+        Demon,
+        Drainer,
+        Angel,
+        Spirit,
+        Mystic
+    }
+
+    public Kind kind { get; private set; }
+    public Color color { get; private set; } // Assuming Color is defined elsewhere
+
+    private int _cooldown;
+    public int cooldown 
+    { 
+        get => _cooldown; 
+        private set => _cooldown = value; 
+    }
+
+    public bool isFainted => cooldown > 0;
+
+    public Mon(Kind kind, Color color, int cooldown = 0)
+    {
+        this.kind = kind;
+        this.color = color;
+        _cooldown = cooldown;
+    }
+
+    public void Faint()
+    {
+        _cooldown = 2;
+    }
+
+    public void DecreaseCooldown()
+    {
+        if (_cooldown > 0)
+        {
+            _cooldown--;
+        }
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Mon mon && this == mon;
+    }
+
+    public bool Equals(Mon other)
+    {
+        return this == other;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(kind, color, _cooldown);
+    }
+
+    public static bool operator ==(Mon left, Mon right)
+    {
+        return left.kind == right.kind && left.color == right.color && left._cooldown == right._cooldown;
+    }
+
+    public static bool operator !=(Mon left, Mon right)
+    {
+        return !(left == right);
+    }
+}
