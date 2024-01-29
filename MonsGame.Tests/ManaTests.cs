6cacfb63457dd42ce5ae19d2efd002170de2045a
ivan grachev <ivan@ivan.lol>
2024-01-29 15:14:12 +0300

    add mana and tests

--- MonsGame.Tests/ManaTests.cs
+++ MonsGame.Tests/ManaTests.cs
@@ -2,3 +2,88 @@
 
 using System.Text.Json;
 using MonsGame;
+
+public class ManaTests
+{
+    [Theory]
+    [InlineData(Color.White)]
+    [InlineData(Color.Black)]
+    public void Regular_CreatesRegularManaWithColor(Color color)
+    {
+        var mana = Mana.Regular(color);
+
+        Assert.Equal(ManaType.Regular, mana.Type);
+        Assert.Equal(color, mana.Color);
+    }
+
+    [Fact]
+    public void Supermana_CreatesSupermanaWithDefaultColor()
+    {
+        var mana = Mana.Supermana;
+
+        Assert.Equal(ManaType.Supermana, mana.Type);
+        Assert.Equal(default(Color), mana.Color);
+    }
+
+    [Theory]
+    [InlineData(Color.White, Color.White, 1)]
+    [InlineData(Color.White, Color.Black, 2)]
+    public void Score_ReturnsCorrectScoreForRegularMana(Color manaColor, Color playerColor, int expectedScore)
+    {
+        var mana = Mana.Regular(manaColor);
+        var score = mana.Score(playerColor);
+
+        Assert.Equal(expectedScore, score);
+    }
+
+    [Theory]
+    [InlineData(Color.White)]
+    [InlineData(Color.Black)]
+    public void Score_ReturnsTwoForSupermana(Color playerColor)
+    {
+        var mana = Mana.Supermana;
+        var score = mana.Score(playerColor);
+
+        Assert.Equal(2, score);
+    }
+
+    [Fact]
+    public void Equals_ReturnsTrueForEqualMana()
+    {
+        var mana1 = Mana.Regular(Color.White);
+        var mana2 = Mana.Regular(Color.White);
+
+        Assert.True(mana1.Equals(mana2));
+    }
+
+    [Fact]
+    public void Equals_ReturnsFalseForDifferentMana()
+    {
+        var mana1 = Mana.Regular(Color.White);
+        var mana2 = Mana.Regular(Color.Black);
+
+        Assert.False(mana1.Equals(mana2));
+    }
+
+    [Fact]
+    public void EqualityOperators_WorkCorrectly()
+    {
+        var mana1 = Mana.Regular(Color.White);
+        var mana2 = Mana.Regular(Color.White);
+        var mana3 = Mana.Regular(Color.Black);
+
+        Assert.True(mana1 == mana2);
+        Assert.False(mana1 == mana3);
+        Assert.False(mana1 != mana2);
+        Assert.True(mana1 != mana3);
+    }
+
+    [Fact]
+    public void GetHashCode_ReturnsSameValueForEqualObjects()
+    {
+        var mana1 = Mana.Regular(Color.White);
+        var mana2 = Mana.Regular(Color.White);
+
+        Assert.Equal(mana1.GetHashCode(), mana2.GetHashCode());
+    }
+}
--- MonsGame/Mana.cs
+++ MonsGame/Mana.cs
@@ -1,3 +1,62 @@
 // ∅ 2024 super-metal-mons
 
 namespace MonsGame;
+
+public enum ManaType
+{
+    Regular,
+    Supermana
+}
+
+public struct Mana : IEquatable<Mana>
+{
+    public ManaType Type { get; private set; }
+    public Color Color { get; private set; } // Assuming Color is defined elsewhere
+
+    private Mana(ManaType type, Color color)
+    {
+        Type = type;
+        Color = color;
+    }
+
+    public static Mana Regular(Color color) => new Mana(ManaType.Regular, color);
+    public static Mana Supermana => new Mana(ManaType.Supermana, default);
+
+    public int Score(Color player)
+    {
+        switch (Type)
+        {
+            case ManaType.Regular:
+                return Color == player ? 1 : 2;
+            case ManaType.Supermana:
+                return 2;
+            default:
+                throw new InvalidOperationException("Unknown Mana Type");
+        }
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Mana mana && this == mana;
+    }
+
+    public bool Equals(Mana other)
+    {
+        return Type == other.Type && Color == other.Color;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Type, Color);
+    }
+
+    public static bool operator ==(Mana left, Mana right)
+    {
+        return left.Type == right.Type && left.Color == right.Color;
+    }
+
+    public static bool operator !=(Mana left, Mana right)
+    {
+    return !(left == right);
+    }
+}
