c012592490fb2e29977b539be2bb1145884d6341
ivan grachev <ivan@ivan.lol>
2024-01-30 15:32:05 +0300

    add rules test case model

---
+++ MonsGame.Tests/RulesTestCase.cs
@@ -0,0 +1,17 @@
+// ∅ 2024 super-metal-mons
+
+using System.Text.Json;
+using MonsGame;
+
+[Serializable]
+public class RulesTestCase
+{
+    public string FenBefore { get; set; } = string.Empty;
+    public List<Input> Input { get; set; } = new List<Input>();
+    public Output Output { get; set; } = new InvalidInputOutput();
+    public string FenAfter { get; set; } = string.Empty;
+
+    public RulesTestCase()
+    {
+    }
+}
