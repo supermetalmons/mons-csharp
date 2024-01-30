5747d3943d8cf1a2b5b1f1d1328a3ede48ca6025
ivan grachev <ivan@ivan.lol>
2024-01-30 13:28:54 +0300

    test game fen

--- MonsGame.Tests/FenRepresentableTests.cs
+++ MonsGame.Tests/FenRepresentableTests.cs
@@ -2,3 +2,23 @@
 
 using System.Text.Json;
 using MonsGame;
+
+public class FenRepresentableTests
+{
+    [Fact]
+    public void GameFromFen_ShouldCreateEquivalentGame()
+    {
+        var initialGame = new Game();
+        string fen = initialGame.Fen;
+        var newGame = Game.FromFen(fen);
+        Assert.Equal(initialGame.WhiteScore, newGame.WhiteScore);
+        Assert.Equal(initialGame.BlackScore, newGame.BlackScore);
+        Assert.Equal(initialGame.ActiveColor, newGame.ActiveColor);
+        Assert.Equal(initialGame.ActionsUsedCount, newGame.ActionsUsedCount);
+        Assert.Equal(initialGame.ManaMovesCount, newGame.ManaMovesCount);
+        Assert.Equal(initialGame.MonsMovesCount, newGame.MonsMovesCount);
+        Assert.Equal(initialGame.WhitePotionsCount, newGame.WhitePotionsCount);
+        Assert.Equal(initialGame.BlackPotionsCount, newGame.BlackPotionsCount);
+        Assert.Equal(initialGame.TurnNumber, newGame.TurnNumber);
+    }
+}
