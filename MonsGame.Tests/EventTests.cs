2f843d7d8f1bda33dd36835f3f6b4aab9ea0cc04
ivan grachev <ivan@ivan.lol>
2024-01-31 14:17:30 +0300

    event json

--- MonsGame.Tests/EventTests.cs
+++ MonsGame.Tests/EventTests.cs
@@ -14,4 +14,61 @@ public class GameOverEventTests
 
         Assert.Equal(expectedWinner, gameOverEvent.Winner);
     }
+
+    [Fact]
+    public void MonMoveEvent_Serialization_Deserialization_Test()
+    {
+        string json = "{\"monMove\":{\"from\":{\"i\":7,\"j\":4},\"to\":{\"i\":5,\"j\":7},\"item\":{\"mon\":{\"mon\":{\"color\":\"black\",\"cooldown\":0,\"kind\":\"spirit\"}}}}}";
+        ValidateSerializationAndDeserialization<Event>(json);
+    }
+
+    [Fact]
+    public void MysticActionEvent_Serialization_Deserialization_Test()
+    {
+        string json = "{\"mysticAction\":{\"mystic\":{\"color\":\"white\",\"cooldown\":0,\"kind\":\"mystic\"},\"from\":{\"i\":10,\"j\":3},\"to\":{\"i\":1,\"j\":8}}}";
+        ValidateSerializationAndDeserialization<Event>(json);
+    }
+
+    [Fact]
+    public void MonFaintedEvent_Serialization_Deserialization_Test()
+    {
+        string json = "{\"monFainted\":{\"mon\":{\"color\":\"black\",\"cooldown\":0,\"kind\":\"demon\"},\"from\":{\"i\":1,\"j\":8},\"to\":{\"i\":0,\"j\":7}}}";
+        ValidateSerializationAndDeserialization<Event>(json);
+    }
+
+    [Fact]
+    public void ManaMoveEvent_Serialization_Deserialization_Test()
+    {
+        string json = "{\"manaMove\":{\"mana\":{\"regular\":{\"color\":\"black\"}},\"from\":{\"i\":4,\"j\":7},\"to\":{\"i\":8,\"j\":3}}}";
+        ValidateSerializationAndDeserialization<Event>(json);
+    }
+
+    [Fact]
+    public void NextTurnEvent_Serialization_Deserialization_Test()
+    {
+        string json = "{\"nextTurn\":{\"color\":\"white\"}}";
+        ValidateSerializationAndDeserialization<Event>(json);
+    }
+
+    [Fact]
+    public void PickupManaEvent_Serialization_Deserialization_Test()
+    {
+        string json = "{\"pickupMana\":{\"mana\":{\"regular\":{\"color\":\"white\"}},\"by\":{\"color\":\"white\",\"cooldown\":0,\"kind\":\"drainer\"},\"at\":{\"i\":7,\"j\":7}}}";
+        ValidateSerializationAndDeserialization<Event>(json);
+    }
+
+    [Fact]
+    public void DemonAdditionalStepEvent_Serialization_Deserialization_Test()
+    {
+        string json = "{\"demonAdditionalStep\":{\"demon\":{\"color\":\"white\",\"cooldown\":0,\"kind\":\"demon\"},\"from\":{\"i\":4,\"j\":6},\"to\":{\"i\":6,\"j\":5}}}";
+        ValidateSerializationAndDeserialization<Event>(json);
+    }
+
+    private void ValidateSerializationAndDeserialization<T>(string json)
+    {
+        var deserialized = JsonSerializer.Deserialize<T>(json, JsonOptions.DefaultSerializerOptions);
+        var serializedBack = JsonSerializer.Serialize(deserialized, JsonOptions.DefaultSerializerOptions);
+        Assert.Equal(json, serializedBack);
+    }
+
 }
--- MonsGame/Event.cs
+++ MonsGame/Event.cs
@@ -2,6 +2,10 @@
 
 namespace MonsGame;
 
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+[JsonConverter(typeof(EventJsonConverter))]
 public abstract class Event
 {
     public enum EventType
@@ -272,4 +276,199 @@ public class GameOverEvent : Event
         Winner = winner;
         Type = EventType.GameOver;
     }
+}
+
+public class EventJsonConverter : JsonConverter<Event>
+{
+    public override Event Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var document = JsonDocument.ParseValue(ref reader);
+        var root = document.RootElement.EnumerateObject().First();
+        var eventType = root.Name;
+        var eventObj = root.Value;
+
+        return eventType switch
+        {
+            "monMove" => new MonMoveEvent(
+                JsonSerializer.Deserialize<Item>(eventObj.GetProperty("item").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("from").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("to").GetRawText(), options)),
+            "mysticAction" => new MysticActionEvent(
+                JsonSerializer.Deserialize<Mon>(eventObj.GetProperty("mystic").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("from").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("to").GetRawText(), options)),
+            "monFainted" => new MonFaintedEvent(
+                JsonSerializer.Deserialize<Mon>(eventObj.GetProperty("mon").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("from").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("to").GetRawText(), options)),
+            "manaMove" => new ManaMoveEvent(
+                JsonSerializer.Deserialize<Mana>(eventObj.GetProperty("mana").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("from").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("to").GetRawText(), options)),
+            "demonAction" => new DemonActionEvent(
+                JsonSerializer.Deserialize<Mon>(eventObj.GetProperty("demon").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("from").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("to").GetRawText(), options)),
+            "demonAdditionalStep" => new DemonAdditionalStepEvent(
+                JsonSerializer.Deserialize<Mon>(eventObj.GetProperty("demon").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("from").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("to").GetRawText(), options)),
+            "spiritTargetMove" => new SpiritTargetMoveEvent(
+                JsonSerializer.Deserialize<Item>(eventObj.GetProperty("item").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("from").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("to").GetRawText(), options)),
+            "pickupBomb" => new PickupBombEvent(
+                JsonSerializer.Deserialize<Mon>(eventObj.GetProperty("by").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("at").GetRawText(), options)),
+            "pickupPotion" => new PickupPotionEvent(
+                JsonSerializer.Deserialize<Item>(eventObj.GetProperty("by").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("at").GetRawText(), options)),
+            "pickupMana" => new PickupManaEvent(
+                JsonSerializer.Deserialize<Mana>(eventObj.GetProperty("mana").GetRawText(), options),
+                JsonSerializer.Deserialize<Mon>(eventObj.GetProperty("by").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("at").GetRawText(), options)),
+            "supermanaBackToBase" => new SupermanaBackToBaseEvent(
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("from").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("to").GetRawText(), options)),
+            "bombAttack" => new BombAttackEvent(
+                JsonSerializer.Deserialize<Mon>(eventObj.GetProperty("by").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("from").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("to").GetRawText(), options)),
+            "monAwake" => new MonAwakeEvent(
+                JsonSerializer.Deserialize<Mon>(eventObj.GetProperty("mon").GetRawText(), options),
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("at").GetRawText(), options)),
+            "bombExplosion" => new BombExplosionEvent(
+                JsonSerializer.Deserialize<Location>(eventObj.GetProperty("at").GetRawText(), options)),
+            "nextTurn" => new NextTurnEvent(
+                JsonSerializer.Deserialize<Color>(eventObj.GetProperty("color").GetRawText(), options)),
+            "gameOver" => new GameOverEvent(
+                JsonSerializer.Deserialize<Color>(eventObj.GetProperty("winner").GetRawText(), options)),
+            _ => throw new JsonException($"Event type {eventType} is not supported.")
+        };
+    }
+
+    public override void Write(Utf8JsonWriter writer, Event value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        writer.WritePropertyName(value.Type.ToString().LowercaseFirst());
+        writer.WriteStartObject();
+
+        switch (value)
+        {
+            case MonMoveEvent monMoveEvent:
+                writer.WritePropertyName("from");
+                JsonSerializer.Serialize(writer, monMoveEvent.From, options);
+                writer.WritePropertyName("to");
+                JsonSerializer.Serialize(writer, monMoveEvent.To, options);
+                writer.WritePropertyName("item");
+                JsonSerializer.Serialize(writer, monMoveEvent.Item, options);
+                break;
+            case MysticActionEvent mysticActionEvent:
+                writer.WritePropertyName("mystic");
+                JsonSerializer.Serialize(writer, mysticActionEvent.Mystic, options);
+                writer.WritePropertyName("from");
+                JsonSerializer.Serialize(writer, mysticActionEvent.From, options);
+                writer.WritePropertyName("to");
+                JsonSerializer.Serialize(writer, mysticActionEvent.To, options);
+                break;
+            case MonFaintedEvent monFaintedEvent:
+                writer.WritePropertyName("mon");
+                JsonSerializer.Serialize(writer, monFaintedEvent.Mon, options);
+                writer.WritePropertyName("from");
+                JsonSerializer.Serialize(writer, monFaintedEvent.From, options);
+                writer.WritePropertyName("to");
+                JsonSerializer.Serialize(writer, monFaintedEvent.To, options);
+                break;
+            case ManaMoveEvent manaMoveEvent:
+                writer.WritePropertyName("mana");
+                JsonSerializer.Serialize(writer, manaMoveEvent.Mana, options);
+                writer.WritePropertyName("from");
+                JsonSerializer.Serialize(writer, manaMoveEvent.From, options);
+                writer.WritePropertyName("to");
+                JsonSerializer.Serialize(writer, manaMoveEvent.To, options);
+                break;
+            case DemonActionEvent demonActionEvent:
+                writer.WritePropertyName("demon");
+                JsonSerializer.Serialize(writer, demonActionEvent.Demon, options);
+                writer.WritePropertyName("from");
+                JsonSerializer.Serialize(writer, demonActionEvent.From, options);
+                writer.WritePropertyName("to");
+                JsonSerializer.Serialize(writer, demonActionEvent.To, options);
+                break;
+            case DemonAdditionalStepEvent demonAdditionalStepEvent:
+                writer.WritePropertyName("demon");
+                JsonSerializer.Serialize(writer, demonAdditionalStepEvent.Demon, options);
+                writer.WritePropertyName("from");
+                JsonSerializer.Serialize(writer, demonAdditionalStepEvent.From, options);
+                writer.WritePropertyName("to");
+                JsonSerializer.Serialize(writer, demonAdditionalStepEvent.To, options);
+                break;
+            case SpiritTargetMoveEvent spiritTargetMoveEvent:
+                writer.WritePropertyName("item");
+                JsonSerializer.Serialize(writer, spiritTargetMoveEvent.Item, options);
+                writer.WritePropertyName("from");
+                JsonSerializer.Serialize(writer, spiritTargetMoveEvent.From, options);
+                writer.WritePropertyName("to");
+                JsonSerializer.Serialize(writer, spiritTargetMoveEvent.To, options);
+                break;
+            case PickupBombEvent pickupBombEvent:
+                writer.WritePropertyName("by");
+                JsonSerializer.Serialize(writer, pickupBombEvent.By, options);
+                writer.WritePropertyName("at");
+                JsonSerializer.Serialize(writer, pickupBombEvent.At, options);
+                break;
+            case PickupPotionEvent pickupPotionEvent:
+                writer.WritePropertyName("by");
+                JsonSerializer.Serialize(writer, pickupPotionEvent.By, options);
+                writer.WritePropertyName("at");
+                JsonSerializer.Serialize(writer, pickupPotionEvent.At, options);
+                break;
+            case PickupManaEvent pickupManaEvent:
+                writer.WritePropertyName("mana");
+                JsonSerializer.Serialize(writer, pickupManaEvent.Mana, options);
+                writer.WritePropertyName("by");
+                JsonSerializer.Serialize(writer, pickupManaEvent.By, options);
+                writer.WritePropertyName("at");
+                JsonSerializer.Serialize(writer, pickupManaEvent.At, options);
+                break;
+            case SupermanaBackToBaseEvent supermanaBackToBaseEvent:
+                writer.WritePropertyName("from");
+                JsonSerializer.Serialize(writer, supermanaBackToBaseEvent.From, options);
+                writer.WritePropertyName("to");
+                JsonSerializer.Serialize(writer, supermanaBackToBaseEvent.To, options);
+                break;
+            case BombAttackEvent bombAttackEvent:
+                writer.WritePropertyName("by");
+                JsonSerializer.Serialize(writer, bombAttackEvent.By, options);
+                writer.WritePropertyName("from");
+                JsonSerializer.Serialize(writer, bombAttackEvent.From, options);
+                writer.WritePropertyName("to");
+                JsonSerializer.Serialize(writer, bombAttackEvent.To, options);
+                break;
+            case MonAwakeEvent monAwakeEvent:
+                writer.WritePropertyName("mon");
+                JsonSerializer.Serialize(writer, monAwakeEvent.Mon, options);
+                writer.WritePropertyName("at");
+                JsonSerializer.Serialize(writer, monAwakeEvent.At, options);
+                break;
+            case BombExplosionEvent bombExplosionEvent:
+                writer.WritePropertyName("at");
+                JsonSerializer.Serialize(writer, bombExplosionEvent.At, options);
+                break;
+            case NextTurnEvent nextTurnEvent:
+                writer.WritePropertyName("color");
+                JsonSerializer.Serialize(writer, nextTurnEvent.Color, options);
+                break;
+            case GameOverEvent gameOverEvent:
+                writer.WritePropertyName("winner");
+                JsonSerializer.Serialize(writer, gameOverEvent.Winner, options);
+                break;
+            default:
+                throw new JsonException($"Event type {value.Type} is not supported.");
+        }
+
+        writer.WriteEndObject();
+        writer.WriteEndObject();
+    }
+
 }
